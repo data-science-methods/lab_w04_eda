a03b66373b13c3014aa5ea7092788f744c7b8fae
Dan Hicks <hicks.daniel.j@gmail.com>
2020-09-13 20:17:49 +0000

    first test

--- lab.R
+++ lab.R
@@ -35,7 +35,7 @@ dataf = read_csv(file.path('data', 'ames.csv'))
 #' 
 #' 1. Look through the short descriptions in `?ames_raw` (or online, <https://cran.r-project.org/web/packages/AmesHousing/AmesHousing.pdf>).  Which variable reports the actual sale price? 
 #' 
-#' 
+#' `Sale_Price`
 #' 
 #' 2. As you were looking through the variable descriptions, you probably noticed a few variables that might be good predictors of sale price.  List two or three here. 
 #' 
@@ -46,23 +46,25 @@ dataf = read_csv(file.path('data', 'ames.csv'))
 #' # Problem 2 #
 #' We've already loaded the data, so let's jump to step 3, "Check the packaging," and 5, "Check your 'n's." Use `skimr::skim()` and `vis_dat` functions to answer the following questions.  To report the values that you find, replace the value assigned to the `problem` variable.  
 #' 
+skim(dataf)
+
 #' 1. The paper abstract (see above) reports 2930 rows.  How many observations (rows) are in our version of the dataset?  
 #' 
-problem2.1 = 1.7e15 # scientific notation: 1.7 x 10^15
+problem2.1 = 2930
 
 #' 2. The abstract also reports 80 "explanatory variables (23 nominal, 23 ordinal, 14 discrete, and 20 continuous)."  In R, factors are used for both nominal and ordinal; and numerics are used for both discrete and continuous. How many of each are in our version?
 #' 
-problem2.2.factors = 7
-problem2.2.characters = 18000
-problem2.2.numerics = 12
+problem2.2.factors = 0
+problem2.2.characters = 46
+problem2.2.numerics = 35
 
 #' 3. Explain any discrepancies here. 
 #' 
-#' 
+#' 1 extra numeric variable, for sale price
 #' 
 #' 4. How many variables have missing values?  
 #' 
-problem2.4 = 937
+problem2.4 = 0
 
 
 #' # Problem 3 #
@@ -75,11 +77,12 @@ dataf %>%
 
 #' 1. Examine the full codebook, at <http://jse.amstat.org/v19n3/decock/DataDocumentation.txt>.  What do the values of MS_Zoning represent? 
 #' 
-#' 
+#' Zoning codes; residential vs. commercial vs. agriculture
 #' 
 #' 2. Run the following two expressions.  Why do they give different results? 
 #' 
-#' 
+#' First one filters out individual houses based on sale price; 
+#' second one filters out zoning types based on mean sale price
 #' 
 dataf %>% 
     group_by(MS_Zoning) %>% 
@@ -101,31 +104,34 @@ dataf %>%
 
 dup_demo = tribble(
     ~x, ~y, 
-     1,  5, 
-     1,  5, 
-     2,  4
+    1,  5, 
+    1,  5, 
+    2,  4
 )
 dup_demo
 distinct(dup_demo)
 
 #' Use this function to create a dataframe `dataf_nodup` with the duplicate rows removed.  Do `dataf` and `dataf_nodup` have the same number of rows?  What does this tell you about duplicate entries in our dataset?  
 #' 
+#' Yes, same number of rows; so no duplicate entries
 #' 
-#' 
-# dataf_nodup = ???
-
+dataf_nodup = distinct(dataf)
+identical(nrow(dataf), nrow(dataf_nodup))
 
 #' # Problem 5 #
 #' *Coding ordinal variables*: R uses the class `factor` to represent categorical (also called "nominal") and ordinal variables.  Because the CSV format doesn't have a way to document the variable type for its columns, when we load a CSV into R the categorical and ordinal variables get parsed as characters rather than factors.  
 #' 
 #' 1. What's the class of the overall condition variable, `Overall_Cond`? 
 #' 
+#' `character`
 #' 
-#' 
+class(dataf$Overall_Cond)
+
 #' 2. Tidyverse functions are generally pretty good about treating a character variable as a factor when it makes sense to do so.  Use `count()` to get a count of the number of houses by overall condition; assign this data frame to `cond_count`. 
 #'  
 
-# cond_count = ???
+cond_count = count(dataf, Overall_Cond)
+cond_count
 
 #' 3. However, notice that the possible values of the variable (called the *levels* of the factor) are in alphabetical order, not from "Very Poor" through "Average" to "Excellent" as intended.  We would see the same thing if we try to coerce `Overall_Cond` to a factor:  
 
@@ -135,17 +141,21 @@ levels(bad_factor)
 
 #' To fix this, we need to set the order of the levels manually.  First, edit the next line, so that the levels correspond to the list in the documentation, with "Very Poor" as the lowest (first) value and "Very Excellent" as the highest (<http://jse.amstat.org/v19n3/decock/DataDocumentation.txt>, search for "Overall Cond").  
 
-condition_levels = c('Above_Average', 'Very_Poor', 'Below_Average')
+condition_levels = c('Very_Poor', 'Poor', 
+                     'Fair', 'Below_Average', 
+                     'Average', 'Above_Average', 
+                     'Good', 'Very_Good', 
+                     'Excellent', 'Very_Excellent')
 
 #' 4. To fix the levels, we use the `levels = ` argument in `factor()`.  Modify the code for `bad_factor` above, assigning the result to `good_factor`.  Confirm that the levels are in the right order. 
 
-# good_factor = ???
+good_factor = factor(dataf$Overall_Cond, levels = condition_levels)
 
 #' 5. Finally we want this factor to be in our analysis dataframe.  *Normally, to preserve immutability*, I would do this right after reading in the CSV (using a pipe).  We'll talk more about this when we work on cleaning data.  For this lab, just do it here.  
 #' 
 #' Using `mutate()`, add a variable `overall_cond_fct` to `dataf`, that represents overall condition as a factor with the levels in the correct order. 
 
-# dataf = ???
+dataf = mutate(dataf, overall_cond_fct = good_factor)
 
 
 #' # Problem 6 #
@@ -163,23 +173,23 @@ select(dataf, Sale_Price, Overall_Cond, Gr_Liv_Area)
 #' 
 #' Fill in the blanks in the following code.  Hints: Use `?factor` and `?numeric` to bring up documentation for these classes.  Look for functions that start with `is`.  Check the documentation for `where()` to see examples. 
 
-# dataf_smol = select(???, where(???), where(???))
+dataf_smol = select(dataf, where(is.factor), where(is.numeric))
 
 #' 2. `cor()` doesn't like factors.  `as.integer()` will coerce a factor into an integer representation; then `method = 'spearman'` will tell `cor()` to use Spearman correlation instead of Pearson correlation.  (Spearman correlation is based on the rank of the variable values, rather than the values directly.  This is a standard approach for dealing with correlations of ordinal variables.)
 #' 
 #' Fill in the blank: 
 
-# cor_matrix = dataf_smol %>%
-#     mutate(???) %>%
-#     cor(method = 'spearman')
+cor_matrix = dataf_smol %>%
+    mutate(overall_cond_fct = as.integer(overall_cond_fct)) %>%
+    cor(method = 'spearman')
 
 #' 3. Now we convert the correlation matrix into a dataframe.  
 #' a. Explain what the following line of code is doing.  Hint: Read the docs! 
 #' 
-#' 
+#' Coercing cor_matrix to a dataframe (tibble), turning the row names into the `covar` variable
 #' 
 
-# cor_df = as_tibble(cor_matrix, rownames = 'covar')
+cor_df = as_tibble(cor_matrix, rownames = 'covar')
 
 #' b. What do the rows of `cor_df` represent?  The columns?  The values in each cell? 
 #' - rows: 
@@ -194,6 +204,10 @@ select(dataf, Sale_Price, Overall_Cond, Gr_Liv_Area)
 #' - Assigns the result to the variable `top_10`
 #' 
 
+top_10 = cor_df %>% 
+    select(covar, Sale_Price) %>% 
+    arrange(desc(Sale_Price)) %>% 
+    top_n(10)
 
 #' # Problem 7 #
 #' In 1.2, you identified some variables that you thought might be good predictors of sale price.  How good were your expectations? 
--- tests/test_lab.R
+++ tests/test_lab.R
@@ -4,35 +4,36 @@ library(testthat)
 warning('Problem 1 not checked automatically')
 
 test_that("2.1. How many observations?", {
-    expect_identical(problem2.1, 2930L)
+    expect_equal(problem2.1, 2930L)
 })
 
 test_that("2.2. How many variables?", {
-    expect_identical(problem2.2.factors, 0L)
-    expect_identical(problem2.2.characters, 46L)
-    expect_identical(problem2.2.numerics, 35L)
+    expect_equal(problem2.2.factors, 0L)
+    expect_equal(problem2.2.characters, 46L)
+    expect_equal(problem2.2.numerics, 35L)
 })
 
 test_that("2.4. Missing values", {
-    expect_identical(problem2.4, 0L)
+    expect_equal(problem2.4, 0L)
 })
 
 warning('Problem 3 not checked automatically')
 
 test_that('4. Duplicate rows', {
-    expect_identical(nrow(dataf_nodup), 2930L)
+    expect_equal(nrow(dataf_nodup), 2930L)
 })
 
 test_that('5.2. Count of houses by overall condition', {
-    expect_identical(nrow(cond_count), 9L)
+    expect_equal(nrow(cond_count), 9L)
 })
 
+correct_levels = c('Very_Poor', 'Poor', 
+                   'Fair', 'Below_Average', 
+                   'Average', 'Above_Average', 
+                   'Good', 'Very_Good', 
+                   'Excellent', 'Very_Excellent')
+
 test_that('5.3. Condition levels', {
-    correct_levels = c('Very_Poor', 'Poor', 
-                       'Fair', 'Below_Average', 
-                       'Average', 'Above_Average', 
-                       'Good', 'Very_Good', 
-                       'Excellent', 'Very_Excellent')
     expect_identical(condition_levels, correct_levels)
 })
 
@@ -41,9 +42,10 @@ test_that('5.4. Overall condition factor with the correct levels', {
                                          levels = correct_levels))
 })
 
+dataf_fct = mutate(dataf, overall_cond_fct = factor(Overall_Cond, 
+                                                    levels = correct_levels))
+
 test_that('5.5. `overall_cond_fct` is a factor in `dataf` with the correct levels', {
-    dataf_fct = mutate(dataf, overall_cond_fct = factor(Overall_Cond, 
-                                                        levels = correct_levels))
     expect_identical(dataf$overall_cond_fct, dataf_fct$overall_cond_fct)
 })
 
